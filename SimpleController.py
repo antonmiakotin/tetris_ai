edf9f41d71c8cae09117d6ee1e439ada9eba2e7e
Anton <anton.miakotin@gmail.com>
2011-04-18 21:02:19 -0500

    added a few comments

--- SimpleController.py
+++ SimpleController.py
@@ -8,8 +8,10 @@ class simple_controller:
     def __init__(self):
         self.board = GameBoard.Board()
         self.pieces = []
+        #generate a list of class names
         shape_classes = [Shapes.square_shape, Shapes.t_shape, Shapes.l_shape, Shapes.reverse_l_shape, Shapes.i_shape]
         
+        #pic 10 random pieces
         for i in range(10):
             cls = choice(shape_classes)
             self.pieces.append(cls)
@@ -20,4 +22,5 @@ class simple_controller:
         return self.pieces
     def get_child_states(self):
         print self.board.landed
+        #just for testing, only worked with square shape
         BoardStates.BoardStates.generate_child_states(self.board, Shapes.square_shape)
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import SimpleController
 
 if __name__ == "__main__":
+    #create controller
     cont = SimpleController.simple_controller()
+    #run states function
     cont.get_child_states()
     
      
