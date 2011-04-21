c59ff430b534118c28b7361d0c0df9a4ac518617
Stephen Kalpin <skalpin@gmail.com>
2011-04-21 18:25:11 -0500

    created main.py for two versions, plus named my algorithm AggressiveSearch

---
+++ AggressiveSearch.py
@@ -0,0 +1,25 @@
+# This method takes a board, a file with a list of pieces, and a threshold.
+# While it plays it will not consider states where lines have been created
+# in order to try and create a tetris every time it clears lines thus scoring
+# the most points
+# Stephen Kalpin
+
+
+# Method runs the algorithm
+@staticmethod
+def run(board, state_list, threshold):
+    # for list of pieces
+    for state in state_list
+    # first grab the first set of states given the parent state
+    # order them by points
+    # if under the threshold
+    # remove the ones where less than 4 lines have been created
+    # else place the ones where a line has been created to the front
+    # remove all but the best 3 choices
+    # if lines have been created calculate the points
+    # expand the results on those 3 choices
+    # as we want to remove those first
+    # loop back and grab the next piece
+    #
+    # after all pieces have been played
+    # return board with highest score
---
+++ main.py
@@ -0,0 +1,68 @@
+import GameBoard
+import BoardStates
+import Shapes
+import AggressiveSearch
+from random import choice
+if __name__ == "__main__":
+    #create game board, fill with some squares to test eval
+    board = GameBoard.Board()
+    
+    #throw some more random blocks
+    board.landed.append((3,18))
+    board.landed.append((2,18))
+    board.landed.append((7,18))
+    board.landed.append((7,17))
+    
+#    f = open("output.txt", 'w')   
+
+    init_state = BoardStates.State(id, board, 0, None)
+    child_states = [init_state]
+    
+    #run the game in file input mode
+    if(len(sys.argv) > 1):
+        if "-L" in sys.argv:    # not that clever, just pass -L as the first argument
+            # and the filename as the second
+
+            # pass in the board and the file name
+            LocalSerach.run(board, sys.argv[2])
+        elif "-P" in sys.argv:
+            i = 0
+            for a in sys.argv:
+                i = i + 1
+                if(a == "-t"):
+                    threshold = a + 1
+                    break
+            AggressiveSearch.run(board, sys.arg[2], threshold) 
+
+        else:
+            print "Usage -[LP] [filename].tgame"
+
+
+    # This shoudl all be moved to LocalSearch.py or something like that
+    # for piece in random_pieces:
+        
+    #     for i in range(len(child_states)):
+    #         #need to remove root nodes from board list
+    #         state = child_states.pop(i)
+    #         #run states function
+    #         result_tuples = BoardStates.BoardStates.generate_child_states(state, piece)
+            
+    #         #sort all boards, highest score first
+    #         result_tuples = sorted(result_tuples, key=lambda state: state[0], reverse = True)
+    #         #pick the top 3
+    #         result_tuples = result_tuples[:30]
+    #         #output to file
+    #         f.write( "BASE STATE\n" )
+    #         f.write( "#"*30+"\n" )
+    #         f.write( str(state) )
+    #         f.write( "CHILD STATES\n" )
+    #         f.write ( "#"*30+"\n" )
+    #         for tup in result_tuples:
+    #             f.write( str(tup[1]) )
+    #             child_states.append(tup[1])
+            
+
+    
+    
+   
+
